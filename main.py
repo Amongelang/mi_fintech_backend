f90f7ef40b7ac2298ead2228cb1a90862c6a4d74
Amongelang <168990791+Amongelang@users.noreply.github.com>
2025-11-23 20:35:17 +0100

    Probar temporalmente /data sin llamar a servicios externos

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-from flask import Flask, jsonify
-from services.openbank_api import get_openbank_data
-
-app = Flask(__name__)
-
-# Página principal para GoCardless
-@app.route('/')
-def home():
-    return "¡Backend funcionando en localhost:5000!"
-
-# Callback para GoCardless
-@app.route('/callback')
-def callback():
-    return "¡Callback de GoCardless recibido!"
-
-# Tu ruta actual de datos
 @app.route("/data")
 def get_data():
-    data_openbank = get_openbank_data()
     result = {
-        "total_balance":  data_openbank['balance'],
-        "accounts": [data_openbank],
-        "transactions": data_openbank['transactions']
+        "total_balance": 1000,
+        "accounts": [
+            {"name": "Cuenta de prueba", "balance": 1000}
+        ],
+        "transactions": [
+            {"date": "2025-11-23", "amount": -50, "description": "Gasto prueba"}
+        ]
     }
     return jsonify(result)
 
-if __name__ == "__main__":
-    # Añadimos host="0.0.0.0" si quieres que ngrok pueda conectarse
-    app.run(debug=True, port=5000, host="0.0.0.0")
 
