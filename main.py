d329d398cd275c1e0657ec05ae87b70ff6c1b680
Amongelang <168990791+Amongelang@users.noreply.github.com>
2025-11-24 00:50:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# main.py
 from flask import Flask, jsonify
 from services.openbank_api import get_openbank_data, get_yapily_banks
 
 app = Flask(__name__)
 
-# Página principal
 @app.route('/')
 def home():
     return "¡Backend funcionando en localhost:5000!"
 
-# Callback (puedes usarlo más tarde para OAuth o pagos)
 @app.route('/callback')
 def callback():
     return "¡Callback recibido!"
 
-# Ruta de datos de prueba
 @app.route("/data")
 def get_data():
     data_openbank = get_openbank_data()
@@ -25,13 +21,9 @@ def get_data():
     }
     return jsonify(result)
 
-# Ruta para obtener bancos de Yapily
-from services.openbank_api import get_openbank_data, get_yapily_banks
-
 @app.route("/banks")
 def banks():
     return jsonify(get_yapily_banks())
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
